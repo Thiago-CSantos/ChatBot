ba4d91ce9e741c56d045fbce76f9b0e7bd92e0d0
Thiago-CSantos <thiagocarretero433@gmail.com>
2024-12-15 21:13:27 -0300

    melhorando comunicação

--- chatbot.js
+++ chatbot.js
@@ -18,111 +18,95 @@ async function sendMessageToQueue(queue, message) {
 
         await channel.assertQueue(queue, { durable: true });
         const isSent = channel.sendToQueue(queue, Buffer.from(message));
-        
+
         if (isSent) {
             console.log(`Mensagem enviada para a fila ${queue}: ${message}`);
-          } else {
+        } else {
             console.log(`Falha ao enviar mensagem para a fila ${queue}`);
-          }
+        }
         // Fecha a conexão
-    await channel.close();
-    await connection.close();
+        await channel.close();
+        await connection.close();
 
     } catch (error) {
         console.error(error);
     }
 }
 
-const delay = ms => new Promise(res => setTimeout(res, ms)); // Função que usamos para criar o delay entre uma ação e outra
+const clientStates = {};
 
 client.on('message', async msg => {
+    try {
+        const chatId = msg.from;
+        const currentState = clientStates[chatId] ? clientStates[chatId].status : 'default';
 
-    if (msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola|teste)/i) && msg.from.endsWith('@c.us')) {
+        switch (currentState) {
+            case "default":
+                if (msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola)/i) && msg.from.endsWith('@c.us')) {
 
-        const chat = await msg.getChat();
+                    const chat = await msg.getChat();
+                    await chat.sendStateTyping(); // Simulando Digitação
+                    const contact = await msg.getContact(); //Pegando o contato
+                    const name = contact.pushname; //Pegando o nome do contato
+                    await client.sendMessage(msg.from, 'Olá! ' + name.split(" ")[0] + ` Sou o assistente virtual da empresa Trincha. 
+                    \nComo posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:
+                    1 - Ver cardápio\n2 - Fazer Pedido`); //Primeira mensagem de texto
 
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        const contact = await msg.getContact(); //Pegando o contato
-        const name = contact.pushname; //Pegando o nome do contato
-        await client.sendMessage(msg.from, 'Olá! ' + name.split(" ")[0] + ' Sou o assistente virtual da empresa tal. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Como funciona\n2 - Valores dos planos\n3 - Benefícios\n4 - Como aderir\n5 - Outras perguntas'); //Primeira mensagem de texto
+                }
 
-        await sendMessageToQueue('chatbot_messages', "Mensagem enviada para o cliente: ");
+                if (msg.body === "2") {
+                    clientStates[chatId] = { status: "fazendoPedido", pedidos: [] };
+                    const chat = await msg.getChat();
+                    await chat.sendStateTyping();
+                    await client.sendMessage(chatId, "Perfeito! Por favor, me diga o que você gostaria de pedir.");
+                }
 
-    }
+                break;
 
-    if (msg.body !== null && msg.body === '1' && msg.from.endsWith('@c.us')) {
-        const chat = await msg.getChat();
+            case "fazendoPedido":
+                // Verifica se o cliente ainda não tem o estado inicial
+                if (!clientStates[chatId]) {
+                    clientStates[chatId] = { status: "fazendoPedido", pedidos: [] };
+                }
 
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Nosso serviço oferece consultas médicas 24 horas por dia, 7 dias por semana, diretamente pelo WhatsApp.\n\nNão há carência, o que significa que você pode começar a usar nossos serviços imediatamente após a adesão.\n\nOferecemos atendimento médico ilimitado, receitas\n\nAlém disso, temos uma ampla gama de benefícios, incluindo acesso a cursos gratuitos');
+                const pedido = msg.body;
 
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'COMO FUNCIONA?\nÉ muito simples.\n\n1º Passo\nFaça seu cadastro e escolha o plano que desejar.\n\n2º Passo\nApós efetuar o pagamento do plano escolhido você já terá acesso a nossa área exclusiva para começar seu atendimento na mesma hora.\n\n3º Passo\nSempre que precisar');
+                if (pedido.match(/(finalizar)/i)) {
+                    const pedidosFinalizados = clientStates[chatId].pedidos.join(", ");
+                    await client.sendMessage(chatId, "Obrigado! Seu pedido foi registrado. Entraremos em contato em breve para confirmação.");
 
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
+                    // Volta para o estado "default" e limpa a lista de pedidos
+                    clientStates[chatId] = { status: "default", pedidos: [] };
 
-    }
+                    const contact = await msg.getContact();
+                    const name = contact.pushname;
+                    await sendMessageToQueue('chatbot_messages', `Pedido recebido de ${name}: ${pedidosFinalizados}`);
+                    break;
+                }
 
-    if (msg.body !== null && msg.body === '2' && msg.from.endsWith('@c.us')) {
-        const chat = await msg.getChat();
+                if (pedido.match(/(adicionar)/i)) {
+                    // Pede para o cliente informar mais um item
+                    await client.sendMessage(chatId, "Por favor, informe o próximo item que você gostaria de adicionar ao seu pedido.");
+                    break;
+                }
 
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, '*Plano Individual:* R$22,50 por mês.');
+                // Adiciona o pedido à lista de pedidos no estado do cliente
+                clientStates[chatId].pedidos.push(pedido);
+                console.log(clientStates[chatId].pedidos);
 
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
-    }
+                await client.sendMessage(chatId, `Recebi o seu pedido: "${pedido}". Caso queira adicionar mais itens, digite "adicionar". Ou digite "finalizar" para concluir.`);
+                break;
 
-    if (msg.body !== null && msg.body === '3' && msg.from.endsWith('@c.us')) {
-        const chat = await msg.getChat();
+            default:
+                console.log(currentState);
 
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Sorteio de em prêmios todo ano.\n\nAtendimento médico ilimitado 24h por dia.\n\nReceitas de medicamentos');
+                await client.sendMessage(chatId, "Desculpe, algo deu errado. Por favor, tente novamente.");
+                clientStates[chatId] = { status: "default", pedidos: [] }; // Garante que o estado volte ao padrão
+                break;
+        }
 
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        console.log("Body", msg.body);
-        console.log("From", msg.from);
-
-    }
-
-    if (msg.body !== null && msg.body === '4' && msg.from.endsWith('@c.us')) {
-        const chat = await msg.getChat();
-
-        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Você pode aderir aos nossos planos diretamente pelo nosso site ou pelo WhatsApp.\n\nApós a adesão, você terá acesso imediato');
-
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
-
-    }
-
-    if (msg.body !== null && msg.body === '5' && msg.from.endsWith('@c.us')) {
-        const chat = await msg.getChat();
-        console.log("Body", msg.body);
-        console.log("From", msg.from);
-
-        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Se você tiver outras dúvidas ou precisar de mais informações, por favor, fale aqui nesse whatsapp ou visite nosso site: https://site.com ');
-
+    } catch (error) {
+        console.error(error);
     }
 
 });
